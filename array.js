32e89286fbc188b894a1037a91a26a368964b859
hmimran031 <hmimran031@hmail.com>
2024-05-13 21:33:25 +0600

    2nd commit

---
+++ array.js
@@ -0,0 +1,13 @@
+/*var city = ["Dhaka", "Barishal", "khulna", "Rangpur"];
+
+console.log(city);
+console.log(city[1]);*/
+
+// Loop use in array
+
+var city = ["Dhaka", "Barishal", "khulna", "Rangpur", "Savar"];
+
+var i;
+for (i = 0; i < city.length; i = i + 1) {
+  console.log(city[i]);
+}
---
+++ object.js
@@ -0,0 +1,35 @@
+//
+var myPc = {
+  brand: "HP",
+  screen: "16'5",
+  islaptop: "true",
+  ram: "8gb",
+  disk: "500gb",
+  isSSD: "true",
+  processor: "core i-5",
+};
+console.log(myPc);
+
+//
+var myPc = {
+  brand: "HP",
+  screen: "16'5",
+  islaptop: "true",
+  ram: "8gb",
+  disk: "500gb",
+  isSSD: "true",
+  processor: "core i-5",
+};
+console.log(myPc["isSSD"]);
+
+//
+var myPc = {
+  brand: "HP",
+  screen: "16'5",
+  islaptop: "true",
+  ram: "8gb",
+  disk: "500gb",
+  isSSD: "true",
+  processor: "core i-5",
+};
+console.log(myPc.brand);
